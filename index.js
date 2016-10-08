29e4af87e5dd12964ada811005f78973ce9fe5d3
Aaron Lipinski <kris.lipinski@gmail.com>
2016-10-08 16:07:39 +1300

    cachy nodes

---
+++ index.js
@@ -0,0 +1,34 @@
+const morphdom = require('morphdom')
+const hyperx = require('hyperx')
+const mobx = require('mobx')
+const bel = require('bel')
+
+const foreverTrue = () => true
+
+function proxyNode (fn) {
+  const proxy = bel`<span onload=${autorun}></span>`
+  proxy.isSameNode = foreverTrue
+
+  fn.node = proxy
+  fn.node.fn = fn
+
+  return proxy
+}
+
+function autorun (node) {
+  mobx.autorun(() => {
+    const fn = node.fn
+    fn.node = morphdom(fn.node, fn())
+  })
+}
+
+function h (tagName, attrs, children) {
+  const proxiedChildren = children && children.map(
+    node => typeof node === 'function' ? proxyNode(node) : node
+  )
+  return bel.createElement(tagName, attrs, proxiedChildren)
+}
+
+const nod = hyperx(h)
+
+module.exports = nod
