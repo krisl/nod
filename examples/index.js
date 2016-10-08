870a1496ba41ba2c8357f1130d26cdf1ba3934b1
Aaron Lipinski <kris.lipinski@gmail.com>
2016-10-08 15:37:39 +1300

    add example

---
+++ examples/index.js
@@ -0,0 +1,44 @@
+const mobx = require('mobx')
+const nod = require('../')
+
+const state = mobx.observable([
+  {text: 'one', style: ''},
+  {text: 'two'},
+  {text: 'six'}
+])
+
+const comp = (data) =>
+  nod`<div style='${data.style}'>${data.text}</div>`
+
+const test = nod`
+  <div>
+    <button onclick=${() => console.log('heyehy')}>click me</button>
+    nod
+    <div>
+      <Test blah=${state[0]}></Test>
+      <div>
+        ${() => nod`
+          <div>
+            ${state.map(x => nod`
+              <object id=${x.text}>${() => comp(x)}</object>
+            `)}
+          </div>
+        `}
+      </div>
+      hi
+    </div>
+  </div>
+`
+
+const style = nod`
+  <style>
+    div {
+      padding: 5px;
+      border: 1px solid grey;
+    }
+  </style>
+`
+document.body.appendChild(style)
+document.body.appendChild(test)
+
+window.state = state
